9a9798a642e1d819fa637ce0065fe97ce7905bc0
Alexey Aniskin <AlexeyA29@yandex.ru>
2019-11-10 16:00:34 +0300

    add search for admin

--- app/admin_my/forms.py
+++ app/admin_my/forms.py
@@ -8,8 +8,7 @@ from app.main_func import utils as main_utils
 class AdminMenu(FlaskForm):
     '''
     Форма перенаправления из админки: Редактирование Работ, Пользователей, Расписание
-    '''
-    
+    '''    
     to_users = SubmitField(_('Клиенты'), render_kw={"class": "button", "type": "submit"})
     to_works = SubmitField(_('Работы'), render_kw={"class": "button ", "type": "button"})
     to_schedule = SubmitField(_('Расписание'),  render_kw={"class" : "button ", "type": "submit"})
@@ -19,59 +18,38 @@ class EditUsersPhoneForm(FlaskForm):
     '''
     Форма телефонов клиента
     '''
-    id_phone = IntegerField(_('ID телефона'),  render_kw={"class" : "fl-text-field", "type": "text"})
-    number_phone_field = IntegerField(_('Номер'),  render_kw={"class" : "fl-text-field", "type": "text"})
-    phone_confirmed_field = IntegerField(_('Подтвержден'), validators=[DataRequired()], render_kw={"class" : "fl-text-field", "type": "checkbox"})
-    date_to_expire_field = DateField (_('Дата до удаления'), validators=[DataRequired()], render_kw={"class" : "fl-text-field", "type": "date"})
-    black_list_field = IntegerField(_('Черный список'),  render_kw={"class" : "fl-text-field", "type": "checkbox"})
-
+    id_phone = IntegerField(_('ID телефона'),  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    number_phone_field = IntegerField(_('Номер'),  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    phone_confirmed_field = IntegerField(_('Подтвержден'), validators=[DataRequired()], render_kw={"class" : "fl-text-field-user-edit", "type": "checkbox"})
+    date_to_expire_field = DateField (_('Дата до удаления'), validators=[DataRequired()], render_kw={"class" : "fl-text-field-user-edit", "type": "date"})
+    black_list_field = IntegerField(_('Черный список'),  render_kw={"class" : "fl-text-field-user-edit", "type": "checkbox"})
+    to_edit_button = SubmitField(_('Редактор'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
 
 class EditUsersForm(FlaskForm):
     '''
-    Форма обработки Клиентов - поиск, удаление, блокировка, создание нового клиента
+    Форма обработки Клиентов - удаление, блокировка, создание нового клиента
     '''
-    id_user = IntegerField(_('ID клиента'),  render_kw={"class" : "fl-text-field", "type": "text"})
-    username_field= StringField(_('Имя клиента'), validators=[DataRequired],  render_kw={"class" : "fl-text-field", "type": "text"})
-    about_me_field =  StringField(_('О клиенте'), validators=[],  render_kw={"class" : "fl-text-field", "type": "text"})
-    email_field = StringField(_('Email'), validators=[],  render_kw={"class" : "fl-text-field", "type": "text"})
-    email_confirmed_field = SelectField(_('Почта проверена'), validators=[DataRequired()], choices=[('0', _('Не подтверждена')), ('1', _('Подтверждена'))], render_kw={"class" : "fl-text-field", "type": "checkbox"})
-    registration_date_field = DateField (_('Дата регистрации'), validators=[DataRequired()], render_kw={"class" : "fl-text-field", "type": "date"})
-   
-    trying_to_enter_new_phone_field = IntegerField(_('Осталось смен телефона'), validators=[DataRequired()], render_kw={"class" : "fl-text-field", "type": "text"})
-    role_field = SelectField(_('Права'), validators=[DataRequired()], choices=[('admin', _('Администратор')), ('user', _('Клиент'))],render_kw={"class" : "fl-text-field"}) 
-    last_seen_field = DateField (_('Последний визит'), validators=[DataRequired()], render_kw={"class" : "fl-text-field", "type": "date"})
-    type_connection_field=SelectField(_('Связь'), validators=[DataRequired()], choices=[('whatsapp', _('WhatsApp')), ('vk', _('ВКонтакте')), ('instagram', _('Instagram')), ('_number_phone', _('Телефон'))],render_kw={"class" : "fl-text-field"}) 
-    #phones  = SelectField(coerce=int)
-    #phone_form_field = FormField(EditUsersPhoneForm)
+    id_user = StringField(_('ID клиента'),  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    username_field= StringField(_('Имя клиента'), validators=[DataRequired],  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    about_me_field =  StringField(_('О клиенте'), validators=[],  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    email_field = StringField(_('Email'), validators=[],  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    email_confirmed_field = SelectField(_('Почта проверена'), validators=[DataRequired()], choices=[('0', _('Не подтверждена')), ('1', _('Подтверждена'))], render_kw={"class" : "fl-text-field-user-edit", "type": "checkbox"})
+    registration_date_field = DateField (_('Дата регистрации'), validators=[DataRequired()], render_kw={"class" : "fl-text-field-user-edit", "type": "date"})
+    
+    trying_to_enter_new_phone_field = IntegerField(_('Осталось смен телефона'), validators=[DataRequired()], render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    role_field = SelectField(_('Права'), validators=[DataRequired()], choices=[('admin', _('Администратор')), ('user', _('Клиент'))],render_kw={"class" : "fl-text-field-user-edit"}) 
+    last_seen_field = DateField (_('Последний визит'), validators=[DataRequired()], render_kw={"class" : "fl-text-field-user-edit", "type": "date"})
+    type_connection_field=SelectField(_('Связь'), validators=[DataRequired()], choices=[('whatsapp', _('WhatsApp')), ('vk', _('ВКонтакте')), ('instagram', _('Instagram')), ('_number_phone', _('Телефон'))],render_kw={"class" : "fl-text-field-user-edit"}) 
+    
+    to_edit_button = SubmitField(_('Редактор'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
+    to_delete_button = SubmitField(_('Удалить'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
+    to_schedule_button = SubmitField(_('Записать'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
 
-#class ScheduleMaster(FlaskForm):
-#    '''
-#    Форма расписание которое видит мастер
-#    '''
-#    id_time = StringField(_('ID тайминга'),  render_kw={"class" : "comment-field", "type": "text"})   
-#            
-#    work_type_field=SelectField(_('Тип работы'), validators=[DataRequired()], choices=[('man', _('Маникюр')), ('ped', _('Педикюр')), ('man_ped', _('Ман+Пед')),('some', _('Другое'))], render_kw={"class" : "comment-field"}) 
-#    price_field=StringField(_('Цена'), validators=[DataRequired()], render_kw={"class" : "comment-field"}, default="0")
-#    
-#    type_connection_field=SelectField(_('Связь'), validators=[DataRequired()], choices=[('whatsapp', _('WhatsApp')), ('vk', _('ВКонтакте')), ('instagram', _('Instagram')), ('_number_phone', _('Телефон'))],render_kw={"class" : "comment-field"}) 
-#
-#    client_field=StringField(_('Клиент'), validators=[DataRequired()], render_kw={"class" : "comment-field"}, default="")
-#    adress_client_field=StringField(_('Адрес'), validators=[DataRequired()], render_kw={"class" : "comment-field"}, default="")
-#    node_field=TextAreaField(_('Примечание'), validators=[DataRequired()], render_kw={"class" : "comment-field"}, default="")
-#    time_empty_field=SelectField(_('Занять'), validators=[DataRequired()], choices=[(1, _('Свободно')), (0, _('Занято'))],render_kw={"class" : "comment-field"}) 
-#    reserve_time_for_client_field = SelectField(_('Резерв времени'), validators=[DataRequired()], choices=[('one', _('Один час')), ('two', _('Два часа')), ('three', _('Три часа'))],render_kw={"class" : "comment-field"})
-#    client_come = SelectField(_('Пришел ли клиент'), validators=[DataRequired()], choices=[(0, _('Нет')), (1, _('Да')),  (2, _('Опоздал'))], render_kw={"class" : "comment-field"}, default=0)
-#
-#    submit = SubmitField(_('Записать'), render_kw={"class": "button"})
-#    cancel_field = SubmitField(_('Отменить'), render_kw={"class": "button fl-cancel-field"})
-#    clear_field = SubmitField(_('Освободить'),  render_kw={"class" : "button clear-field", "type": "submit"})
-#
-#    def validate_price_field(self, price_field):
-#        
-#        if main_utils.is_digit(price_field.data) == False:
-#            raise ValidationError(_l('Можно вводить только числа'))
-#
-#        price = float(price_field.data)
-#        
-#        if price< 0 or price > 10000:            
-#            raise ValidationError(_l('Цена не может быть отрицательной или больше 10000'))
+class RouterUserForm(FlaskForm):
+    '''
+    Форма поиска Клиентов - поиск, удаление, блокировка, создание нового клиента
+    '''
+    find_field= StringField(_('Имя/телефон/права'), validators=[],  render_kw={"class" : "fl-text-field-user-edit", "type": "text"})
+    to_find_button = SubmitField(_('Поиск'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
+    to_create_button = SubmitField(_('Новый'), render_kw={"class": "button fl-button-field-user-edit", "type": "submit"})
+       
--- app/admin_my/routes.py
+++ app/admin_my/routes.py
@@ -1,8 +1,10 @@
-﻿from flask import render_template, url_for, redirect, request
+﻿import re
+from flask import render_template, url_for, redirect, request
+from datetime import datetime
 from app.admin_my import bp
 from app.decorators.decorators import admin_required
 from flask_babel import _
-from app.admin_my.forms import AdminMenu, EditUsersForm, EditUsersPhoneForm
+from app.admin_my.forms import AdminMenu, EditUsersForm, EditUsersPhoneForm, RouterUserForm
 from app.user.models import User, UserPhones, ConnectionType
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -18,76 +20,89 @@ def admin_index():
         return redirect(url_for('master_schedule.show_schedule_master'))
 
     if form_admin_menu.to_users.data:        
-        return redirect(url_for('admin_my.edit_users_form', index_client = "-1"))
+        return redirect(url_for('admin_my.find_users', time_date_id = '-1'))
 
     return render_template('admin_my/index.html', title=titleVar, form_admin_menu=form_admin_menu)
 
-@bp.route('/edit_users_form<index_client>', methods=['GET', 'POST'])
+@bp.route('/find_users_form_<time_date_id>', methods=['GET', 'POST'])
 @admin_required
-def edit_users_form(index_client):
+def find_users(time_date_id = -1):
     '''
     Вывод страницы редактирования клиентов
     '''
+    print(time_date_id)
     titleVar='Редактирование клиентов'
     list_edit_users_form = []
-    index_client = int(index_client)
+    find_form = RouterUserForm()
+    users = []
+
+    try:
+        time_date_id = int(time_date_id)
+    except:
+        time_date_id = -1
 
     if request.method == "POST":
-        pass
-    elif request.method == "GET":
-        if index_client < 0:
+        if find_form.to_find_button.data:
+            result = find_form.find_field.data
+            users = list(set(User.query.filter(UserPhones.user_id == User.id).filter(UserPhones.number.like(f'%{str(result)}%')).all() + \
+            User.query.filter(User.username.like(f'%{result}%')).all() + \
+            User.query.filter(User.role.like(f'%{result}%')).all()))
+            if find_form.find_field.data == "" or None:
+                users =  User.query.all()
+
+        if find_form.to_create_button.data:
+            pass
+        
+    elif request.method == "GET":        
+            #отображать всех клиентов
             users =  User.query.all()
-            for u in users:
-                conection_type = ConnectionType.query.filter(ConnectionType.id == u.connection_type_id).first()
+            find_form.find_field.data = ""
 
-                edit_users_form = EditUsersForm()
-                list_phones = {}
-              #  edit_users_form = EditUsersForm(request.POST, obj=u)
+    for u in users:
+        conection_type = ConnectionType.query.filter(ConnectionType.id == u.connection_type_id).first()
 
-                edit_users_form.id_user.data = u.id
-                edit_users_form.username_field.data=u.username
-                edit_users_form.about_me_field.data =  u.about_me
-                edit_users_form.email_field.data = u.email
-                if u.email_confirmed == 0:
-                    edit_users_form.email_confirmed_field.data = "0"
-                else:
-                    edit_users_form.email_confirmed_field.data = "1"                
-                edit_users_form.registration_date_field.data = u.registration_date  
-                edit_users_form.trying_to_enter_new_phone_field.data = u.trying_to_enter_new_phone
-                
-                if u.role == 'admin':
-                    edit_users_form.role_field.data = 'admin'
-                else:
-                    edit_users_form.role_field.data = 'user'
-               
-                edit_users_form.last_seen_field.data = u.last_seen
-                if conection_type:
-                    edit_users_form.type_connection_field.data=conection_type.name_of_type               
-                
-                for p in u.phones:
-                    form_phone = EditUsersPhoneForm()
-                    form_phone.id_phone.data = p.id
-                    form_phone.number_phone_field.data = p.number
-                    form_phone.black_list_field.data = p.black_list
-                    form_phone.phone_confirmed_field.data = p.phone_checked
-                    form_phone.date_to_expire_field.data = p.expire_date_hash
+        edit_users_form = EditUsersForm()
+        list_phones = {}
+      #  edit_users_form = EditUsersForm(request.POST, obj=u)
 
-                    list_phones={ p : form_phone}
+        edit_users_form.id_user.data = u.id
+        edit_users_form.username_field.data=u.username
+        edit_users_form.about_me_field.data =  u.about_me
+        edit_users_form.email_field.data = u.email
+        if u.email_confirmed == 0:
+            edit_users_form.email_confirmed_field.data = "0"
+        else:
+            edit_users_form.email_confirmed_field.data = "1"                
+        edit_users_form.registration_date_field.data = u.registration_date if u.registration_date else datetime.utcnow()
+        edit_users_form.trying_to_enter_new_phone_field.data = u.trying_to_enter_new_phone
+        
+        if u.role == 'admin':
+            edit_users_form.role_field.data = 'admin'
+        else:
+            edit_users_form.role_field.data = 'user'
+       
+        edit_users_form.last_seen_field.data = u.last_seen if u.last_seen else datetime.utcnow()
+        if conection_type:
+            edit_users_form.type_connection_field.data=conection_type.name_of_type               
+        
+        for p in u.phones:
+            form_phone = EditUsersPhoneForm()
+            form_phone.id_phone.data = p.id
+            form_phone.number_phone_field.data = p.number
+            form_phone.black_list_field.data = p.black_list
+            form_phone.phone_confirmed_field.data = p.phone_checked
+            form_phone.date_to_expire_field.data = p.expire_date_hash if p.expire_date_hash else datetime.utcnow()
 
-              #  phones= u.phones
-              #  choices = []
-              #  for p in phones:
-              #      choices.append((p.id, p.number))                    
-              #  #print((s.id) for s in choices)
-              #  edit_users_form.phones.choices = choices
-              #  #edit_users_form.phones.choices = [(p.id, p.number) for p in UserPhones.query.filter(UserPhones.user_id == u.id).order_by('number')]
-                
+            list_phones={ p : form_phone}
 
-                #edit_users_form.phones.data = u.phones
+        list_edit_users_form.append((edit_users_form, list_phones))           
 
 
+    return render_template('admin_my/edit_users.html', title=titleVar, time_date_id=time_date_id, find_form=find_form, list_edit_users_form=list_edit_users_form)
 
-                list_edit_users_form.append((edit_users_form, list_phones))           
-
-
-        return render_template('admin_my/edit_users.html', title=titleVar, list_edit_users_form=list_edit_users_form)
+@bp.route('/edit_users_form_<index_client>', methods=['GET', 'POST'])
+@admin_required
+def edit_users_form(index_client):
+    '''
+    Маршрут создания или редактирования пользователя
+    '''
